e143fe7f7489c5f45b11b21ef9747d50273ca4ff
LunaStev <youngjae681@gmail.com>
2025-06-13 17:40:43 +0900

    Update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     i18n: {
-        locales: ['en', 'ko', 'es', 'zh_CH', 'de', 'fr', 'tr', 'pt', 'ja', 'ms'],
+        locales: ['en', 'ko', 'es', 'zh_CH', 'de', 'fr', 'tr', 'pt', 'ja', 'ms', 'pl', 'ru', 'hi', 'uk'],
         defaultLocale: 'en',
     },
 }
